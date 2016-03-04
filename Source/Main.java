f57a2aeaa74a94a553ae217b31cf0f7cb583dff8
Michael Boimistruck <michaelb_38@hotmail.com>
2016-03-04 00:38:52 -0500

    Update Main.java

--- Source/Main.java
+++ Source/Main.java
@@ -1,9 +1,31 @@
-// Fix the URL to autofill the month/year
+import java.io.*;
+import java.util.Random;
 
-public class Main extends GeneratePairs {
+public class Main {
+
+  static String vowels[][] = {{"ㅏ", "ㅓ", "ㅗ", "ㅜ", "ㅡ", "ㅣ", "ㅑ","ㅕ", "ㅛ", "ㅠ"},
+                                      {"a","eo","o","u","eu","i","ya","yeo","yo","yu"}};
+
+  static String consonants[][] = {{"ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ","ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"},
+                                        {"g","n","d","r/l","m","b","s","silent/..ng","j","ch","k","t", "p", "h"}};
+
+  static Random rand = new Random();
+
+  public static void output() {
+    int randomNum = rand.nextInt((8 - 0) + 1) + 1;
+    System.out.println("Hangul: " +vowels[0][randomNum]);
+    System.out.println("Roman:  " +vowels[1][randomNum]);
+    System.out.println();
+
+  }
 
   public static void main(String[] args) {
-    GeneratePairs.output();
+    int i = 10;
 
+    while (i > 0){
+      output();
+      i--;
+
+    }
   }
 }
