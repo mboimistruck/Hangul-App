bbf8653d4823c34395faa33f4bc82f33999a73d3
Michael Boimistruck <michaelb_38@hotmail.com>
2016-03-03 23:42:35 -0500

    adding

---
+++ Source/GeneratePairs.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import javax.swing.JOptionPane;
+
+public class GeneratePairs {
+
+  public static void output() {
+    JOptionPane.showMessageDialog(null, "s");
+
+  }
+}
---
+++ Source/Main.java
@@ -0,0 +1,9 @@
+// Fix the URL to autofill the month/year
+
+public class Main extends GeneratePairs {
+
+  public static void main(String[] args) {
+    GeneratePairs.output();
+
+  }
+}
